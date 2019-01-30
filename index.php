5277a16b9df23bfeca8324f0c3511879a7314b78
João Batista Gomes Silva <jhonxbatista@gmail.com>
2019-01-30 13:10:23 -0300

    alterações do estilo com bootstrap

--- index.php
+++ index.php
@@ -19,35 +19,54 @@ if(isset($_POST['generatePDF'])) {
 </head>
 <body>
 	<div class="container">
-		<div class="row">
-			
+
 			<form method="POST">
-				<div class="col-md-4">
-					<div class="form-group">
-						<label>Nome: </label>
-						<input type="text" name="nome" class="form-control">
+				<div class="col-md-6 col-md-offset-3" style="margin-top: 20px;">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							Formulário para emição de GRU - Restaurante Universitário
+						</div>
+
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-12">	
+									<div class="form-group">
+										<label>Nome: </label>
+										<input type="text" name="nome" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-md-12">	
+									<div class="form-group">
+										<label>CPF: </label>
+										<input type="text" name="CPF" class="form-control">
+									</div>
+								</div>	
+							</div>
+							
+							<div class="row">
+								<div class="col-md-12">	
+									<div class="form-group">
+										<label>Valor: </label>
+										<input type="number" name="valor" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-md-12">	
+									<button type="submit" name="generatePDF" class="btn btn-primary btn-block" style="margin-top:  22px;">
+										<span class="glyphicon glyphicon-print"></span> &nbsp;
+										Gerar
+									</button>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group">
-						<label>CPF: </label>
-						<input type="text" name="CPF" class="form-control">
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group">
-						<label>Valor: </label>
-						<input type="number" name="valor" class="form-control">
-					</div>
-				</div>
-				<div class="col-md-2">
-					<button type="submit" name="generatePDF" class="btn btn-success btn-block" style="margin-top:  22px;">
-						<span class="glyphicon glyphicon-ok-sign"></span> &nbsp;
-						Gerar
-					</button>
 				</div>
 			</form>
-			
 		</div>
 	</div>
 </body>
